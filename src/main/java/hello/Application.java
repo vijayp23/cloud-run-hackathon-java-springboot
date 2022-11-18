adf1c8e51968ddd13f29196c2f45cdf0c54b3946
Vijay Patil <vijayp.github@gmail.com>
2022-11-18 14:00:10 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -55,23 +55,27 @@ public class Application {
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    System.out.println(arenaUpdate);
+    System.out.println(arenaUpdate.toString());
     //String[] commands = new String[]{"F", "R", "L", "T"};
 
-//    Arena myArena = arenaUpdate.arena;
-//    Integer x = myArena.dims.get(0);
-//    Integer y = myArena.dims.get(1);
-//
-//    List<Integer> arenaDims = myArena.dims;
-//
-//
-//    PlayerState myState = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
-//    Integer myScore = myState.score;
+    Arena myArena = arenaUpdate.arena;
+    Integer x = myArena.dims.get(0);
+    Integer y = myArena.dims.get(1);
 
+    List<Integer> arenaDims = myArena.dims;
+
+
+    PlayerState myState = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    Integer myX = myState.x;
+    Integer myY = myState.y;
+    String myD = myState.direction;
     String[] commands = new String[]{"F", "R", "L", "T", "T", "T", "T", "T", "T", "T", "T","T","T"};
 
     int i = new Random().nextInt(12);
 
+//    if (myX == 0 && myD.equals())
+//
+//    if (myX == 0 && myState.direction == "")
 
     return commands[i];
 
