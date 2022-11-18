c6c4132505841e7a85431587a7165a25008536ef
Vijay Patil <vijayp.github@gmail.com>
2022-11-18 14:35:43 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -58,88 +58,90 @@ public class Application {
 //    System.out.println(arenaUpdate.arena);
     //String[] commands = new String[]{"F", "R", "L", "T"};
 
-    Arena myArena = arenaUpdate.arena;
-    Integer x = myArena.dims.get(0);
-    Integer y = myArena.dims.get(1);
+//    Arena myArena = arenaUpdate.arena;
+//    Integer x = myArena.dims.get(0);
+//    Integer y = myArena.dims.get(1);
+//
+//    List<Integer> arenaDims = myArena.dims;
+//
+//
+//    PlayerState myState = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+//    Integer myX = myState.x;
+//    Integer myY = myState.y;
+//    String myD = myState.direction;
+//    String[] commands = new String[]{"F", "R", "L", "T", "T", "T", "T", "T", "T", "T", "T","T","T"};
+//
+//    int i = new Random().nextInt(12);
+////    System.out.println(myX);
+////    System.out.println(myY);
+////    System.out.println(myD);
+//
+//    String myRes = commands[i];
+//
+//
+//    if (myX == 0 && myD.equals("E") && myRes.equals("T"))
+//    {
+//      if (myY == 0)     //top left
+//      {
+//        return "L";
+//      }
+//
+//      if  (myY.equals(arenaDims.get(1)))
+//      {     //Bottom left
+//        return "R";
+//      }
+//
+//      return "L";
+//    }
+//
+//    if (myY == 0 && myD.equals("N") && myRes.equals("T"))
+//    {
+//      if (myX == 0)     //top left
+//      {
+//        return "R";
+//      }
+//
+//      if  (myX.equals(arenaDims.get(0)))
+//      {     //top Right
+//        return "L";
+//      }
+//
+//      return "R";
+//    }
+//
+//    if (myX.equals(arenaDims.get(0)) && myD.equals("W") && myRes.equals("T"))
+//    {
+//      if (myY == 0)     //top Right
+//      {
+//        return "R";
+//      }
+//
+//      if  (myY.equals(arenaDims.get(1)))
+//      {     //Bottom Right
+//        return "L";
+//      }
+//
+//      return "L";
+//    }
+//
+//    if (myY.equals(arenaDims.get(1)) && myD.equals("S") && myRes.equals("T"))
+//    {
+//      if (myX == 0)     //Bottom left
+//      {
+//        return "L";
+//      }
+//
+//      if  (myX.equals(arenaDims.get(0)))
+//      {     //Bottom Right
+//        return "R";
+//      }
+//
+//      return "R";
+//    }
 
-    List<Integer> arenaDims = myArena.dims;
-
-
-    PlayerState myState = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
-    Integer myX = myState.x;
-    Integer myY = myState.y;
-    String myD = myState.direction;
     String[] commands = new String[]{"F", "R", "L", "T", "T", "T", "T", "T", "T", "T", "T","T","T"};
-
     int i = new Random().nextInt(12);
-    System.out.println(myX);
-    System.out.println(myY);
-    System.out.println(myD);
-    String myRes = commands[i];
-
-
-    if (myX == 0 && myD.equals("E") && myRes.equals("T"))
-    {
-      if (myY == 0)     //top left
-      {
-        return "L";
-      }
-
-      if  (myY.equals(arenaDims.get(1)))
-      {     //Bottom left
-        return "R";
-      }
-
-      return "L";
-    }
-
-    if (myY == 0 && myD.equals("N") && myRes.equals("T"))
-    {
-      if (myX == 0)     //top left
-      {
-        return "R";
-      }
-
-      if  (myX.equals(arenaDims.get(0)))
-      {     //top Right
-        return "L";
-      }
-
-      return "R";
-    }
-
-    if (myX.equals(arenaDims.get(0)) && myD.equals("W") && myRes.equals("T"))
-    {
-      if (myY == 0)     //top Right
-      {
-        return "R";
-      }
-
-      if  (myY.equals(arenaDims.get(1)))
-      {     //Bottom Right
-        return "L";
-      }
-
-      return "L";
-    }
-
-    if (myY.equals(arenaDims.get(1)) && myD.equals("S") && myRes.equals("T"))
-    {
-      if (myX == 0)     //Bottom left
-      {
-        return "L";
-      }
-
-      if  (myX.equals(arenaDims.get(0)))
-      {     //Bottom Right
-        return "R";
-      }
-
-      return "R";
-    }
-
-
-    return myRes;
+    return commands[i];
 
   }
 
